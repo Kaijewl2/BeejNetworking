66c6f0bb3e1b0ba1d2b149c8df0aa68399734663
kaijewl2 <146896923+Kaijewl2@users.noreply.github.com>
2025-12-17 20:59:05 -0500

    Complete C++ IPv6 retrieval

--- showip.cpp
+++ showip.cpp
@@ -7,24 +7,31 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+// Cheeky bad practice ;(
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  struct addrinfo hints, *res, *pointer;
-  int status;
-  char ipStrC[INET6_ADDRSTRLEN];
+  struct addrinfo hints, *res,
+      *pointer; // hints for getaddrinfo hints arg, res holds response of
+                // getaddrinfo, and pointer holds next struct of linked list
+  int status;   // Determines outcome of getaddrinfo call
+  char ipStrC[INET6_ADDRSTRLEN]; // Holds cheeky C string of IPv6
 
+  // Incorrect input format
   if (argc != 2) {
     cout << "Two arguments, just two is all I ask!" << endl;
   }
 
+  // Spiffy up hints for getaddrinfo
   hints = {};
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_family = AF_INET6;
 
+  // Two for one; call getaddrinfo and hold return value for failure handling
   status = getaddrinfo(argv[1], NULL, &hints, &res);
 
+  // Invalid website received
   if (status != 0) {
     cout << "Go look it up if you don't believe me: '" << argv[1]
          << "' is not a real website! Give me a proper one and I'll tell you "
@@ -33,19 +40,23 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  // Iterate through each retrieved IPv6 from getaddrinfo call
   for (pointer = res; pointer != NULL; pointer = pointer->ai_next) {
 
-    void *addr;
-    struct sockaddr_in6 *address;
+    void *addr; // Pointer to IPv6 field of addressStruct
+    auto *addressStruct = reinterpret_cast<struct sockaddr_in6 *>(
+        pointer->ai_addr); // Gosh I love C++
 
-    address = (struct sockaddr_in6 *)pointer->ai_addr;
-    addr = &(address->sin6_addr);
+    addr = &(addressStruct->sin6_addr); // Set equal to memory location of IPv6
+                                        // in addressStruct
 
-    inet_ntop(pointer->ai_family, addr, ipStrC, sizeof ipStrC);
-    cout << argv[1] << " IP: " << ipStrC << endl;
+    inet_ntop(pointer->ai_family, addr, ipStrC,
+              sizeof ipStrC); // Convert addr into cheeky C string
+    cout << argv[1] << " IP: " << ipStrC
+         << endl; // Print cheeky C string to stream
   }
 
-  freeaddrinfo(res);
+  freeaddrinfo(res); // Free memory
 
   return 0;
 }
